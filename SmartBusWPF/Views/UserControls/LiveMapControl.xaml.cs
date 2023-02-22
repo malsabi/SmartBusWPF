dc1a99aad36e615a039635f921dc49a59db10192
malsabi <xxdarklord65xx@gmail.com>
2023-02-22 16:42:34 +0400

    Added Modal, Added WebView2

--- SmartBusWPF/Views/UserControls/LiveMapControl.xaml.cs
+++ SmartBusWPF/Views/UserControls/LiveMapControl.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.ComponentModel;
+using System.Windows.Controls;
 using Microsoft.Web.WebView2.Core;
 
 namespace SmartBusWPF.Views.UserControls
@@ -13,14 +17,25 @@ namespace SmartBusWPF.Views.UserControls
 
         private async void InitializeWebView()
         {
-            CoreWebView2EnvironmentOptions options = new CoreWebView2EnvironmentOptions("--allow-insecure-localhost");
-            CoreWebView2Environment environment = await CoreWebView2Environment.CreateAsync(null, null, options);
-            await webview.EnsureCoreWebView2Async(environment);
-            webview.CoreWebView2.PermissionRequested += PermissionRequestedHandler;
+            if (!DesignerProperties.GetIsInDesignMode(this))
+            {
+                CoreWebView2EnvironmentOptions options = new CoreWebView2EnvironmentOptions("--allow-insecure-localhost");
+                CoreWebView2Environment environment = await CoreWebView2Environment.CreateAsync(null, null, options);
+                await webview.EnsureCoreWebView2Async(environment);
+                webview.CoreWebView2.PermissionRequested += PermissionRequestedHandler;
+                webview.CoreWebView2.ContextMenuRequested += ContextMenuRequestedHandler;
+                webview.Source = new Uri(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Assets", "Html", "RealTimeMap.html"));
+            }
         }
+
         private void PermissionRequestedHandler(object sender, CoreWebView2PermissionRequestedEventArgs e)
         {
             e.State = CoreWebView2PermissionState.Allow;
+        }
+
+        private void ContextMenuRequestedHandler(object sender, CoreWebView2ContextMenuRequestedEventArgs e)
+        {
+            e.Handled = true;
         }
     }
 }
